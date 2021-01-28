2f4ce04e18dadc30786d978963f276a9c7ec719a
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-27 19:05:25 -0600

    Update services

--- services/students.js
+++ services/students.js
@@ -8,21 +8,18 @@ async function getStudents(){
 
 async function postStudent(id, firstname, lastname)
 {
-    //console.log(id, firstname, lastname)
     let sql=`INSERT INTO dataStudents(id,firstname,lastname) values (${id}, '${firstname}', '${lastname}')`
     await exec.execute(sql)
 }
 
 async function updateStudent(id, firstname, lastname)
 {
-    console.log(id, firstname, lastname)
     let sql=`UPDATE dataStudents SET firstname='${firstname}', lastname='${lastname}' WHERE id=${id}`
     await exec.execute(sql)
 }
 
 async function deleteStudent(id)
 {
-    //console.log(id)
     let sql=`DELETE FROM dataStudents WHERE id = ${id}`
     await exec.execute(sql)
 }

50ff30dcbb726640121a018268ef968cb84da3f4
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-30 13:02:57 -0600

    fix bug of test file

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,11 +1,24 @@
 const request = require('supertest')
 const app = require('../index')
 
-it('respond with json contaning a list of all students', done =>{
-    request(app)
-    .get('/v1/student')
-    .auth('rol', 'admin')
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200, done)
+/*describe('GET', () =>{
+    it('respond with json contaning a list of all students', async done0 =>{
+        request(app)
+        .get('/v1/student')
+        //.set('Accept', 'application/json')
+        .then(done => {expect(done.statusCode).toBe(200)
+        done0()})
+        //.expect('Content-Type', /json/)
+        //.expect(200, done)
+    })
+})*/
+
+describe("GET", ()=>{
+    it('respond with json a list of products', done => {
+        request(app)
+            .get('/v1/students')
+            .set('Accept','application/json')
+            .expect('Content-Type', /json/)
+            .expect(200,done)
+     })
 })
